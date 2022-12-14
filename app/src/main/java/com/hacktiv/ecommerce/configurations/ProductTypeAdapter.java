4235b907e731bfa8b5545f56e98ca092e82449d1
fajar <fajar.wira1403@gmail.com>
2022-12-14 10:52:51 +0700

    Clothing product

--- app/src/main/java/com/hacktiv/ecommerce/configurations/ProductTypeAdapter.java
+++ app/src/main/java/com/hacktiv/ecommerce/configurations/ProductTypeAdapter.java
@@ -2,6 +2,7 @@ package com.hacktiv.ecommerce.configurations;
 
 
 import android.content.Context;
+import android.content.Intent;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -14,12 +15,15 @@ import androidx.recyclerview.widget.RecyclerView;
 
 import com.bumptech.glide.Glide;
 import com.hacktiv.ecommerce.R;
+import com.hacktiv.ecommerce.activities.HomepageActivity;
+import com.hacktiv.ecommerce.activities.product.ClothingProductActivity;
 import com.hacktiv.ecommerce.models.ProductType;
 
 
 import java.util.List;
 
-public class ProductTypeAdapter extends RecyclerView.Adapter<ProductTypeAdapter.ViewHolder> {
+public class
+ProductTypeAdapter extends RecyclerView.Adapter<ProductTypeAdapter.ViewHolder> {
 
     List<ProductType> list;
     Context context;
@@ -48,14 +52,6 @@ public class ProductTypeAdapter extends RecyclerView.Adapter<ProductTypeAdapter.
                 .into(holder.gridImage);
 //        holder.categories.setText(categories.get(position));
 //        holder.gridImage.setImageResource(images.get(position));
-//        holder.cardView.setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View v) {
-//                Intent intent;
-//                intent = new Intent(context, category_activity.class);
-//                context.startActivity(intent);
-//            }
-//        });
     }
 
     @Override
@@ -63,7 +59,7 @@ public class ProductTypeAdapter extends RecyclerView.Adapter<ProductTypeAdapter.
         return list.size();
     }
 
-    public class ViewHolder extends RecyclerView.ViewHolder {
+    public class ViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener{
         TextView categories;
         ImageView gridImage;
         CardView cardView;
@@ -73,6 +69,27 @@ public class ProductTypeAdapter extends RecyclerView.Adapter<ProductTypeAdapter.
             categories = itemView.findViewById(R.id.textView);
             gridImage = itemView.findViewById(R.id.imageView);
             cardView = itemView.findViewById(R.id.cardView);
+            context = itemView.getContext();
+        }
+
+        @Override
+        public void onClick(View view) {
+
+            final Intent intent;
+            switch (getAdapterPosition()) {
+                case 0:
+                    intent = new Intent(context, ClothingProductActivity.class);
+                    break;
+
+                case 1:
+                    intent = new Intent(context, ClothingProductActivity.class);
+                    break;
+
+                default:
+                    intent = new Intent(context, ClothingProductActivity.class);
+                    break;
+            }
+                context.startActivity(intent);
         }
     }
 
